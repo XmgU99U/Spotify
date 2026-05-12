91b103ecdc6e432a0d800fcaa5b06af1450524e5
XmgU99U <gg2768398@gmail.com>
2026-05-12 18:40:06 +0300

    register implement

--- src/auth/authControllers.ts
+++ src/auth/authControllers.ts
@@ -2,12 +2,54 @@ import userModel from "./userModel";
 import { hash, compare } from "bcrypt";
 import { Request, Response } from "express";
 import { generateAccessToken, generateRefreshToken } from "./token_generators";
+import { isEmail, isPassword, isUserName, isCode } from "./validators";
 
-const register = (req: Request, res: Response) => {
+const register = async (req: Request, res: Response) => {
   const { userName, userEmail, userPassword } = req.body;
-  const code : number = Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
-  if(!userName || !userEmail || !userPassword) {
-    return res.sendStatus(400)
+
+  //! get random number
+  const code: number = Math.floor(
+    Math.random() * (999999 - 100000 + 1) + 100000,
+  );
+
+  //! check if one of inputs are undefined
+  if (!userName || !userEmail || !userPassword) {
+    return res.sendStatus(400);
   }
 
+  //! check of one of those inputs are not valid
+  if (!isEmail) {
+    return res.status(400).json({ error: "Invalid email" });
+  }
+  if (!isPassword) {
+    return res.status(400).json({ error: "Invalid password" });
+  }
+  if (!isUserName) {
+    return res.status(400).json({ error: "Invalid username" });
+  }
+
+  //! find a user who has same email
+  const foundedUserWithSameEmail = await userModel
+    .findOne({ userEmail })
+    .exec();
+  if (foundedUserWithSameEmail) {
+    return res.status(400).json({ error: "User already exist" });
+  }
+
+  //! find user who has same username
+  const foundedUserWithSameUserName = await userModel
+    .findOne({ userName })
+    .exec();
+  if (foundedUserWithSameUserName) {
+    return res.status(400).json({ error: "This username already exist" });
+  }
+
+  //! if everything is valid store the data inside database
+  const user = new userModel({
+    userName,
+    userEmail,
+    userPassword,
+    code,
+  });
+  await user.save();
 };
