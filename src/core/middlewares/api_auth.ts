bc7c6d1abcc67cad824207d54d28cdc35c7ab733
XmgU99U <gg2768398@gmail.com>
2026-05-12 22:09:38 +0300

    refactor

--- src/core/middlewares/api_auth.ts
+++ src/core/middlewares/api_auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import {
   JsonWebTokenError,
   JwtPayload,
@@ -8,11 +8,11 @@ import {
 } from "jsonwebtoken";
 
 class ApiAuth {
-  static apiAuth: ApiAuth = new ApiAuth()
-  bearerToken(req: Request, res: Response, next: Function) {
+  static apiAuth: ApiAuth = new ApiAuth();
+  bearerToken(req: Request, res: Response, next: NextFunction) {
     const bearer = req.headers["authorization"];
 
-    if (!bearer || !bearer.startsWith('Bearer')) {
+    if (!bearer || !bearer.startsWith("Bearer")) {
       return res.sendStatus(400);
     }
 
@@ -27,41 +27,40 @@ class ApiAuth {
       res.locals.isVerified = (payload as JwtPayload).isVerified;
       next();
     } catch (e) {
-      return this.handleVerifyTokenError(e, res);
+      if (e instanceof TokenExpiredError) {
+        return res.status(403).json({ error: e.message });
+      }
+      if (e instanceof JsonWebTokenError) {
+        return res.status(401).json({ error: e.message });
+      }
+      if (e instanceof NotBeforeError) {
+        return res.status(401).json({ error: e.message });
+      }
     }
   }
 
-  private handleVerifyTokenError(e: unknown, res: Response) {
-    if (e instanceof JsonWebTokenError) {
-      return res.status(401).json({ error: e.message });
-    }
-    if (e instanceof TokenExpiredError) {
-      return res.status(403).json({ error: e.message });
-    }
-    if (e instanceof NotBeforeError) {
-      return res.status(401).json({ error: e.message });
-    }
-  }
+  basicAuth(req: Request, res: Response, next: Function) {
+    const authHeader = req.headers["authorization"];
 
-
-  basicAuth(req: Request , res: Response , next: Function) {
-        const authHeader = req.headers["authorization"];
-    
     if (!authHeader || !authHeader.startsWith("Basic ")) {
-        return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     const base64Credentials = authHeader.split(" ")[1];
-    const credentials = Buffer.from(base64Credentials, "base64").toString("utf-8");
+    const credentials = Buffer.from(base64Credentials, "base64").toString(
+      "utf-8",
+    );
     const [username, password] = credentials.split(":");
 
-    if (username === process.env.BASIC_AUTH_USERNAME && password === process.env.BASIC_AUTH_PASSWORD) {
-        return next();
+    if (
+      username === process.env.BASIC_AUTH_USERNAME &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return next();
     }
 
     return res.status(401).json({ message: "Invalid credentials" });
   }
 }
 
-
-export default ApiAuth.apiAuth; 
+export default ApiAuth.apiAuth;
