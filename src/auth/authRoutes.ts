6596adb63a780e3b3f7618ec9f83ced584e0ef79
XmgU99U <gg2768398@gmail.com>
2026-05-12 23:36:51 +0300

    add refresh token route

--- src/auth/authRoutes.ts
+++ src/auth/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { register , login , checkCode} from "./authControllers";
+import { register , login , checkCode , refreshAccessToken} from "./authControllers";
 import apiAuth from "../core/middlewares/api_auth";
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.post('/register' , register)
 
 router.post('/checkCode' , apiAuth.bearerToken , checkCode) ;
 
-router.post('/refreshToken' , apiAuth.basicAuth , )
+router.post('/refreshToken' , apiAuth.basicAuth , refreshAccessToken)
 
 
 export default router;
