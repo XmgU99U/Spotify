aace2999508a908214d085f16b2e3a16b5e6f12a
XmgU99U <gg2768398@gmail.com>
2026-05-12 22:01:51 +0300

    add check code route

--- src/auth/authRoutes.ts
+++ src/auth/authRoutes.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { register , login } from "./authControllers";
+import { register , login , checkCode} from "./authControllers";
+import apiAuth from "../core/middlewares/api_auth";
 const router = express.Router();
 
 router.post('/login', login) 
 
 router.post('/register' , register)
+
+router.post('/checkCode' , apiAuth.bearerToken , checkCode) ;
 
 
 export default router;
