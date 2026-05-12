839852fe1226b1a02e65b43019ebe2b68db2cb66
XmgU99U <gg2768398@gmail.com>
2026-05-12 19:18:01 +0300

    login and register logic

--- src/auth/authControllers.ts
+++ src/auth/authControllers.ts
@@ -7,53 +7,105 @@ import { isEmail, isPassword, isUserName, isCode } from "./validators";
 const register = async (req: Request, res: Response) => {
   const { userName, userEmail, userPassword } = req.body;
 
-  
   //! get random number
   const code: number = Math.floor(
     Math.random() * (999999 - 100000 + 1) + 100000,
   );
-  
+
   //! check if one of inputs are undefined
   if (!userName || !userEmail || !userPassword) {
     return res.sendStatus(400);
   }
-  
-  //! check of one of those inputs are not valid
-  if (!isEmail) {
+
+  //! check of one of inputs are not valid
+  if (!isEmail(userEmail)) {
     return res.status(400).json({ error: "Invalid email" });
   }
-  if (!isPassword) {
+  if (!isPassword(userPassword)) {
     return res.status(400).json({ error: "Invalid password" });
   }
-  if (!isUserName) {
+  if (!isUserName(userName)) {
     return res.status(400).json({ error: "Invalid username" });
   }
-  
+
   //! find a user who has same email
   const foundedUserWithSameEmail = await userModel
-  .findOne({ userEmail })
-  .exec();
+    .findOne({ userEmail })
+    .exec();
   if (foundedUserWithSameEmail) {
     return res.status(400).json({ error: "User already exist" });
   }
-  
+
   //! find user who has same username
   const foundedUserWithSameUserName = await userModel
-  .findOne({ userName })
+    .findOne({ userName })
     .exec();
-    if (foundedUserWithSameUserName) {
-      return res.status(400).json({ error: "This username already exist" });
-    }
+  if (foundedUserWithSameUserName) {
+    return res.status(400).json({ error: "This username already exist" });
+  }
 
-  //! hash the password before storing it 
-  const hashedPass = await hash(userPassword , 10);
-    
-  //! if everything is valid store the data inside database
-  const user = new userModel({
+  //! hash the password before storing
+  const hashedPass = await hash(userPassword, 10);
+
+  //! everything is valid
+  const result = await userModel.insertOne({
     userName,
     userEmail,
-    hashedPass,
+    userPassword,
     code,
   });
-  await user.save();
+  const userId = result.id;
+  console.log(userId);
+
+  //! generate tokens
+  const accessTokens = generateAccessToken({ userId });
+  const refreshTokens = generateRefreshToken({ userId });
+
+  //! send success response
+  res.status(201).json({ accessTokens, refreshTokens });
+};
+
+const login = async (req: Request, res: Response) => {
+  const { userEmail, userPassword } = req.body;
+
+  //! check if one of inputs are undefined
+  if (!userEmail || !userPassword) {
+    return res.sendStatus(400);
+  }
+
+  //! check of one of inputs are not valid
+  if (!isEmail(userEmail)) {
+    return res.status(400).json({ error: "Invalid email" });
+  }
+  if (!isPassword(userPassword)) {
+    return res.status(400).json({ error: "Invalid password" });
+  }
+
+  //! get user
+  const findedUserEmail = await userModel.findOne(
+    { $and: [{ isVerified: true }, { userEmail }] },
+    { userPassword: 1 },
+  );
+
+  //! if no user found
+  if (!findedUserEmail) {
+    return res.status(400).json({ error: "Wrong user information" });
+  }
+
+  const userId = findedUserEmail.id;
+
+  //! compare passwords
+  const isMatch = await compare(userPassword, findedUserEmail.userPassword);
+
+  //! if not matches
+  if (!isMatch) {
+    return res.status(400).json({ error: "Wrong user information" });
+  }
+
+  //! generate tokens
+  const accessTokens = generateAccessToken({ userId });
+  const refreshTokens = generateRefreshToken({ userId });
+
+  //! send success responses
+  res.status(200).json({ accessTokens, refreshTokens });
 };
