4fbdd30544fb7347bd0496ad5cb7a245ce2198ae
XmgU99U <gg2768398@gmail.com>
2026-05-12 14:59:04 +0300

    file upload

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
-//!  MY MIDDLE WARES 
+//!  MY MIDDLE WARES
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-
-
-
+app.use(fileUpload());
 
 app.listen(PORT, () => {
-console.log(`SERVER IS RUNNING ON PORT PORT`);
+  console.log(`SERVER IS RUNNING ON PORT PORT`);
 });
