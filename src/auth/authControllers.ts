c39466f2f77e62b5db497132938fb1fafbafddbd
XmgU99U <gg2768398@gmail.com>
2026-05-12 17:44:31 +0300

    auth controller changes

--- src/auth/authControllers.ts
+++ src/auth/authControllers.ts
@@ -1,6 +1,13 @@
 import userModel from "./userModel";
-import {hash , compare} from 'bcrypt' ;
+import { hash, compare } from "bcrypt";
+import { Request, Response } from "express";
+import { generateAccessToken, generateRefreshToken } from "./token_generators";
 
+const register = (req: Request, res: Response) => {
+  const { userName, userEmail, userPassword } = req.body;
+  const code : number = Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
+  if(!userName || !userEmail || !userPassword) {
+    return res.sendStatus(400)
+  }
 
-
-
+};
