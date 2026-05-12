964758d02339ceeb60fa3266d886947fee140316
XmgU99U <gg2768398@gmail.com>
2026-05-12 17:08:19 +0300

    db connect

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import fileUpload from "express-fileupload";
+import { connect } from "mongoose";
+connect("mongodb://localhost:27017/Spotify").then((_data) =>console.log("Connected to DB"));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
